7addf041e858fce56f9c975eff0b4ad45423699d
Ajay Sharma <ajaysharma388>
2020-04-04 00:15:03 +0530

    update.2020.04.04.02

---
+++ Recursion and Backtracking/Lec4_fast_power_recursion.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define pb(x) push_back(x)
+#define ppb() pop_back()
+#define endl "\n"
+
+void fastio() {
+	#ifndef ONLINE_JUGDE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+	#endif	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	return;
+}
+
+ll power(ll a,ll b) {
+	if(b == 0) return 1;
+	if(b == 1) return a;
+	int ans = power(a,b/2);
+	ans *= ans;
+	return (b & 1) ? a*ans : ans;
+} 
+
+int main(){
+	fastio();
+	ll a, b;
+	cin >> a >> b;
+	cout << a << "^" << b << " = " << power(a,b) << endl;
+	return 0;
+}
+
+
+// Sample Input :
+
+// 8 3
+
+// Sample Output :
+
+// 8^3 = 512
