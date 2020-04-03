890701a0f7f2f082d304e1f7eb58e187f37f7b28
Ajay Sharma <ajaysharma388>
2020-04-04 00:53:24 +0530

    update.2020.04.04.02

--- Recursion and Backtracking/Lec4_fast_power_recursion.cc
+++ Recursion and Backtracking/Lec4_fast_power_recursion.cc
@@ -20,19 +20,31 @@ void fastio() {
 	return;
 }
 
+// Time Complexity : O(B)
+// Space Complexity : O(B)
+
 ll power(ll a,ll b) {
+	return (b == 0) ? 1 : a * power(a,b-1);
+}
+
+// Time Complexity : O(Log2B)
+// Space Complexity : O(Log2B)
+
+ll fastPower(ll a,ll b) {
 	if(b == 0) return 1;
 	if(b == 1) return a;
-	int ans = power(a,b/2);
+	int ans = fastPower(a,b/2);
 	ans *= ans;
 	return (b & 1) ? a*ans : ans;
 } 
+
 
 int main(){
 	fastio();
 	ll a, b;
 	cin >> a >> b;
 	cout << a << "^" << b << " = " << power(a,b) << endl;
+	cout << a << "^" << b << " = " << fastPower(a,b) << endl;
 	return 0;
 }
 
