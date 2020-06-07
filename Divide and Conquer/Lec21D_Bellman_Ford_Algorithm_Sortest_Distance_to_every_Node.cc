f1cb0ef8f70589521a1956b9a43b1fb623823191
Ajay Sharma <ajaysharma388>
2020-06-08 01:20:24 +0530

    update-2020.06.08.01

--- Divide and Conquer/Lec21D_Bellman_Ford_Algorithm_Sortest_Distance_to_every_Node.cc
+++ Divide and Conquer/Lec21D_Bellman_Ford_Algorithm_Sortest_Distance_to_every_Node.cc
@@ -1,6 +1,6 @@
 /*==========================
 Author : Ajay Sharma
-date : 03.06.2020
+date : 08.06.2020
 ============================*/
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,8 +8,9 @@ using namespace std;
 // Some useful macros that are helpful to reduce code size.
 
 // this is slightly better implementation of the bellmann ford algorithm
-// for finding the minimum distace from Src to every node in the graph.
-// time complexity is of the order O(V.E) in the worst case.
+// for finding whether there exist any negative weight cycle in the graph
+// or not time complexity is of the order O(V.E) & if cycle exist we will 
+// Simply print the vectices which are in the cycle.
 // where V is the number of vertices in the graph.
 // and E is the number of edges.
 
@@ -41,15 +42,7 @@ struct edge
     int u, v, cost;    
 };
 
-
-bool compare(int a,int b) {
-    cout << a << " < - > " << b << endl;
-    return a > b; 
-}
-
-int32_t main()
-{
-    fastio(true);
+void solve() {
     int n, m, s, d;
     cin >> n >> m >> s >> d;
     int dis[n];
@@ -67,43 +60,80 @@ int32_t main()
     }
     dis[s] = 0;
     path[s] = s;
-    while(true) {
-        bool anyChange = false;
+    int x;
+    for(int phase = 1; phase <= n; ++phase) {
+        x = -1;
         for(int e = 0; e < m; ++e) {
             edge cur = edgesList[e];
             if(dis[cur.u] != INT_MAX and dis[cur.v] > dis[cur.u]+cur.cost) {
-                anyChange = true;
                 path[cur.v] = cur.u;
                 dis[cur.v] = dis[cur.u]+cur.cost;
+                x = cur.u;
             }
         }
-        if(!anyChange) break;
     }
-    vector<char> route;
-    int cur = d;
-    while(path[cur] != cur) {
+    if(x == -1) {
+        cout << "There is No negative weight cycle in the Graph.\nThe route from " 
+        << (char) (s+'A') << " to " << (char) (d+'A') << " is : ";
+        vector<char> route;
+        int cur = d;
+        while(path[cur] != cur) {
+            route.pb(char('A'+cur));
+            cur = path[cur];
+        }
         route.pb(char('A'+cur));
-        cur = path[cur];
+        reverse(route.begin(), route.end());
+        for(auto v : route) cout << v << " -> ";
+    } else {
+        int y = x;
+        for (int i=0; i<n; ++i) {
+            y = path[y];
+        }
+        vector<int> cyclicPath;
+        for (int cur=y; ; cur=path[cur]) {
+            cyclicPath.push_back (cur);
+            if (cur == y && cyclicPath.size() > 1)
+                break;
+        }
+        reverse (cyclicPath.begin(), cyclicPath.end());
+        cout << "Negative cycle: ";
+        for (size_t i=0; i < cyclicPath.size(); ++i)
+            cout << cyclicPath[i] << ' ';
     }
-    route.pb(char('A'+cur));
-    for(int i = 0; i < route.size()/2; ++i) {
-        swap(route[i],route[route.size()-i-1]);
-    }
-    for(auto v : route) cout << v << " -> ";
+    return;
+}
+
+int32_t main()
+{
+    fastio(true);
+    solve();
     return 0;
 }
 
 // Smaple Output :
 
-// 6 7 0
-// A B 4
-// A C 2
-// B C 5
-// B D 10
-// E D 4
-// D F 11
-// C E 3
+// 6 7 0 5
+// 0 1 4
+// 0 2 2
+// 1 2 5
+// 1 3 10
+// 4 3 4
+// 3 5 11
+// 2 4 3
+
+// 4 5 0 3
+// 0 1 5
+// 0 2 4
+// 2 1 -6
+// 1 3 3
+// 3 2 2
+
+
 
 // Sample Output :
 
-// A -> C -> E -> D -> F -> 
+// There is No negative weight cycle in the Graph.
+// The route from A to F is : A -> C -> E -> D -> F -> 
+
+
+// Negative cycle: 1 3 2 1 
